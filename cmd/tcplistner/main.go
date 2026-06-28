5ebbf09e6b47a6a809b6fd5580641c52a9e9e365
valkyraycho <razor0424@gmail.com>
2026-06-28 15:52:37 +0800

    feat: modify main to use request line parser

--- cmd/tcplistner/main.go
+++ cmd/tcplistner/main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
-	"io"
 	"log"
 	"net"
+
+	"github.com/valkyraycho/my-http-from-tcp/internal/request"
 )
 
 func main() {
@@ -13,47 +13,22 @@ func main() {
 	if err != nil {
 		log.Fatal("error", err)
 	}
-	
+
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
 			log.Fatal("error", err)
 		}
 
-		for line := range getLinesChannel(conn) {
-			fmt.Printf("read: %s\n", line)
-		}
-	}
-
-}
-
-func getLinesChannel(f io.ReadCloser) <-chan string {
-	out := make(chan string, 1)
-	go func() {
-		defer f.Close()
-		defer close(out)
-
-		str := ""
-	for {
-		data := make([]byte, 8)
-		n, err := f.Read(data)
-
+		r, err := request.RequestFromReader(conn)
 		if err != nil {
-			break
+			log.Fatal("error", err)
 		}
-		data = data[:n]
-		if i := bytes.IndexByte(data, '\n'); i != -1 {
-			str += string(data[:i])
-			data = data[i+1:]
-			out <- str
-			str = ""
-		}
-		str += string(data)
-	}
-	if len(str) != 0 {
-		out <- str
-	}
-	}()
 
-	return  out
+		fmt.Println("Request Line:")
+		fmt.Printf("- Method: %s\n", r.RequestLine.Method)
+		fmt.Printf("- Request Target: %s\n", r.RequestLine.RequestTarget)
+		fmt.Printf("- HTTP Version: %s\n", r.RequestLine.HttpVersion)
+	}
+
 }
